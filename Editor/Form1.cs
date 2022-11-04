85ad3a72079401063f2eb55f8589c43f86682478
Alberto-6 <66850211+Alberto-6@users.noreply.github.com>
2022-11-04 14:38:55 -0400

    Agregar archivos de proyecto.

---
+++ Editor/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    public partial class Form1 : Form
+    {
+        string archivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abirirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFile = new OpenFileDialog();
+        }
+    }
+}
